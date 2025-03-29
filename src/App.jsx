ab8fab8aead6f298d895846bfc45eaeacf14f2c1
Charles <cchester1@angelo.edu>
2025-03-29 13:20:50 -0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+function App() {
+  // set the global state of characters to be passed down to game component
+  let [brian, setBrian] = useState({
+    health: 100,
+    strength: 10
+  })
+  let [goblin, setGoblin] = useState({
+    health: 0,
+    strength: 0
+  })
+
+  // when user clicks "start game", route to the "game component"
+  return (
+    <div className='flex justify-center align-middle items-center h-screen w-screen'>
+      <div>
+        <button type="button" className='bg-amber-400 text-2xl text-white hover:bg-amber-200 p-3 rounded-xl outline-2 outline-offset-2 outline-blue-500'>
+          Start Game
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const goblins = [
+      {
+            name: "Greesh",
+            health: 35,
+            strength: [3, 4, 5]
+      },
+      {
+            name: "Malloric",
+            health: 45,
+            strength: [4, 6, 8]
+      },
+      {
+            name: "Hecta",
+            health: 50,
+            strength: [5, 7, 9]
+      },
+      {
+            name: "Kenessuk",
+            health: 65,
+            strength: [7, 9, 13]
+      },
+      {
+            name: "Balrog Tamer, Gorak",
+            health: 99,
+            strength: [11, 18, 22]
+      }
+]
+
+export const potions = [
+      {
+            color: "red",
+            type: "health",
+            points: 20
+      },
+      {
+            color: "green",
+            type: "health",
+            points: 25
+      },
+      {
+            color: "blue",
+            type: "strength",
+            points: 10
+      },
+      {
+            color: "orange",
+            type: "strength",
+            points: 20
+      }
+];
