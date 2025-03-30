5cf5e6fa4bb50ae890ad8bb5a95d221b8001f49a
Charles <cchester1@angelo.edu>
2025-03-30 10:25:05 -0500

    potion state and images added to brian container

--- src/components/BrianContainer.jsx
+++ src/components/BrianContainer.jsx
@@ -1,6 +1,6 @@
 // import Brians state and pass as props
-export default function BrianContainer({brian}) {
-      console.log(brian)
+export default function BrianContainer({ brian }) {
+      console.log(brian);
 
       return (
             <div className="grid grid-cols-1 border-2 border-amber-300 m-3">
@@ -12,7 +12,30 @@ export default function BrianContainer({brian}) {
                               className="h-[60px] w-[60px]"
                         />
                   </div>
-                  <div className="flex justify-center">Health: {brian.health}</div>
+                  <div className="flex justify-center">
+                        Health: {brian.health}
+                  </div>
+                  <div className="grid grid-cols-4 justify-center items-center ml-3">
+                        <div>
+                              <img src="/potion_red.png" alt="red potion" />
+                              <span>{brian.potions.red}</span>
+                        </div>
+                        <div>
+                              <img src="/potion_green.png" alt="green potion" />
+                              <span>{brian.potions.green}</span>
+                        </div>
+                        <div>
+                              <img src="/potion_blue.png" alt="blue potion" />
+                              <span>{brian.potions.blue}</span>
+                        </div>
+                        <div>
+                              <img
+                                    src="/potion_orange.png"
+                                    alt="orange potion"
+                              />
+                              <span>{brian.potions.orange}</span>
+                        </div>
+                  </div>
             </div>
       );
 }
