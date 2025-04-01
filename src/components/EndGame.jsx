541dcdd49184ad08ae22a44eec8ed760f796026d
Charles <cchester1@angelo.edu>
2025-04-01 13:56:49 -0500

    styling to game over page

--- src/components/EndGame.jsx
+++ src/components/EndGame.jsx
@@ -1,22 +1,20 @@
 import { useNavigate } from "react-router-dom";
 
-export default function EndGame () {
+export default function EndGame() {
       const navigate = useNavigate();
       // when user clicks "start game", route to the "game component"
       return (
-            <div className="flex justify-center align-middle items-center h-screen w-screen">
-                  <div>
-                        <div className="w-screen flex justify-center">
-                              <p>Brian has perished! Game Over!</p>
-                        </div>
-                        <button
-                              type="button"
-                              className="bg-amber-400 text-2xl text-white hover:bg-amber-200 p-3 rounded-xl outline-2 outline-offset-2 outline-blue-500"
-                              onClick={() => navigate('/')}
-                        >
-                              Restart
-                        </button>
-                  </div>
+            <div className="flex flex-col justify-center items-center h-screen text-center">
+                  <p className="mb-10 text-white text-2xl font-bold">
+                        Brian has perished! Game Over!
+                  </p>
+                  <button
+                        type="button"
+                        className="bg-amber-400 text-2xl text-white hover:bg-amber-200 p-3 rounded-xl outline-2 outline-offset-2 outline-blue-500 w-48"
+                        onClick={() => navigate("/")}
+                  >
+                        Restart
+                  </button>
             </div>
       );
-};
+}
